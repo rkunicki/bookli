9e58e37016ae4d2dd9722f3d0b0840ce5c73343d
Rafał Kunicki <rafalkunicki8@gmail.com>
2022-03-26 20:27:20 +0100

    Installed SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates")
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 if __name__ == "__main__":
