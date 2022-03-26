735cbc3c2c81d4e98b6be09574146b551013e961
Rafał Kunicki <rafalkunicki8@gmail.com>
2022-03-26 23:21:03 +0100

    Changed nullable variable in class Books and added main and manage html files

--- app.py
+++ app.py
@@ -8,18 +8,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+# Create a table in the database
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(240), unique=True, nullable=False)
     author = db.Column(db.String(120), unique=False, nullable=False)
     publishedDate = db.Column(db.DateTime, unique=False, nullable=False)
-    isbn = db.Column(db.Integer, unique=True, nullable=False)
-    pageCount = db.Column(db.Integer, unique=False, nullable=False)
-    previewLink = db.Column(db.String(240), unique=True, nullable=False)
+    isbn = db.Column(db.Integer, unique=True, nullable=True)
+    pageCount = db.Column(db.Integer, unique=False, nullable=True)
+    previewLink = db.Column(db.String(240), unique=True, nullable=True)
     language = db.Column(db.String(20), unique=False, nullable=False)
 
 
 db.create_all()
+
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
 
 if __name__ == "__main__":
     app.run()
