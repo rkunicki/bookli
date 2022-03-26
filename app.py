eb30a1b119e067cf132d591d4f55e35e7eb10fa6
Rafał Kunicki <rafalkunicki8@gmail.com>
2022-03-26 21:07:08 +0100

    Created 'books' table in the database

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(240), unique=True, nullable=False)
+    author = db.Column(db.String(120), unique=False, nullable=False)
+    publishedDate = db.Column(db.DateTime, unique=False, nullable=False)
+    isbn = db.Column(db.Integer, unique=True, nullable=False)
+    pageCount = db.Column(db.Integer, unique=False, nullable=False)
+    previewLink = db.Column(db.String(240), unique=True, nullable=False)
+    language = db.Column(db.String(20), unique=False, nullable=False)
+
+
+db.create_all()
+
 if __name__ == "__main__":
     app.run()
     # app.run(debug=True)
