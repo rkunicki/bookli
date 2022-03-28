280319b1be7256e43ab64f6d66f4f495a964e71f
Rafał Kunicki <rafalkunicki8@gmail.com>
2022-03-28 11:16:09 +0200

    Improved def add_book

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class Books(db.Model):
     title = db.Column(db.String(240), unique=True, nullable=False)
     author = db.Column(db.String(120), unique=False, nullable=False)
     publishedDate = db.Column(db.DateTime, unique=False, nullable=False)
-    isbn = db.Column(db.Integer, unique=True, nullable=False)
+    isbn = db.Column(db.String(13), unique=True, nullable=False)
     pageCount = db.Column(db.Integer, unique=False, nullable=False)
     imageLink = db.Column(db.String(240), unique=True, nullable=False)
     language = db.Column(db.String(20), unique=False, nullable=False)
@@ -48,20 +48,23 @@ def manage():
 def add_book():
     title = request.form.get("title")
     author = request.form.get("author")
-    # published_date = datetime.strptime(request.form.get("publishedDate"), "%Y-%m-%d").date()
     published_date = request.form.get("publishedDate")
     isbn = request.form.get("isbn")
     page_count = request.form.get("pageCount")
     image_link = request.form.get("imageLink")
     language = request.form.get("language")
 
+    if len(isbn) > 13:
+        print("Błąd! ISBN powinien składać się z 10 lub 13 cyfr")
+        return render_template("error-isbn.html")
+
     database = db.session.query(Books).all()
-    all_books = [row.title for row in database]  # List of all books title from database by use comprehensive list
+    all_books = [row.isbn for row in database]  # List of all books title from database by use comprehensive list
     if title and author and published_date and isbn and page_count and image_link and language:
 
         published_date = try_parsing_date(published_date)
 
-        if title in all_books:
+        if isbn in all_books:
             print("Błąd! Podana książka jest już w bazie")
             return render_template("error-database.html")
 
